016ea8b56209b4ab9a7a5648971fbd8675422a19
Friscxion <erwann.champion@outlook.fr>
2021-09-22 14:25:02 +0200

    Button

--- cocorico/manager.js
+++ cocorico/manager.js
@@ -49,9 +49,10 @@ class Manager {
 
         // Level must be stable for 10 ms before an alert event is emitted.
         button.glitchFilter(10000);
-
+        let pass=true;
         button.on('alert', (level, tick) => {
-            if (level === 0) {
+            if (level === 0 && pass) {
+                pass=false;
                 this.setEtat("open");
             }
         });
