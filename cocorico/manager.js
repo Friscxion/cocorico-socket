6e9f6c915f96f870cccaa3c37bb590c852a80a5e
Friscxion <erwann.champion@outlook.fr>
2021-09-25 16:55:17 +0200

    Button

--- cocorico/manager.js
+++ cocorico/manager.js
@@ -55,6 +55,11 @@ class Manager {
         let interval=setInterval(() => {
             motor.servoWrite(pulseWidth);
             pulseWidth += increment;
+            if (pulseWidth >= 2000) {
+                increment = -100;
+            } else if (pulseWidth <= 1000) {
+                increment = 100;
+            }
             if(!pass)
                 clearInterval(interval);
         }, 100);
