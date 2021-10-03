6460444a2e02c3d54a5e80f4118eb2c7dc11021e
Friscxion <erwann.champion@outlook.fr>
2021-10-03 11:48:06 +0200

    Button

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ const Gpio = require('pigpio').Gpio;
 
 const forward = new Gpio(14, {mode: Gpio.OUTPUT});
 const back = new Gpio(15, {mode: Gpio.OUTPUT});
-let dutyCycle = 0;
 
 forward.pwmWrite(100);
 back.pwmWrite(0);
