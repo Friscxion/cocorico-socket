a0e1df751bd362e6d854048e0d38e45d8fbb9761
Friscxion <erwann.champion@outlook.fr>
2021-10-03 11:52:05 +0200

    Button

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
 const Gpio = require('pigpio').Gpio;
+const prompt = require('prompt-sync')({sigint: true});
 
 const forward = new Gpio(14, {mode: Gpio.OUTPUT});
 const back = new Gpio(15, {mode: Gpio.OUTPUT});
 
 forward.pwmWrite(100);
 back.pwmWrite(0);
+
+forward.start(0)
+back.start(0)
+
+while(true){
+    if(prompt){
+        back.stop();
+        forward.stop();
+    }
+}
