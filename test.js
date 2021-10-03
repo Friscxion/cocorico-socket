2c8283a3865cee9b766f6f3832ae8efbf7d6a44f
Friscxion <erwann.champion@outlook.fr>
2021-10-03 12:12:53 +0200

    Button

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 const rpio = require("rpio");
 const IN1=14;
 const IN2=15;
+let options = {
+    gpiomem: false,          /* Use /dev/gpiomem */
+    mapping: 'gpio',    /* Use the P1-P40 numbering scheme */
+    close_on_exit: true,    /* On node process exit automatically close rpio */
+}
 
+rpio.init(options)
 rpio.open(IN1,rpio.PWM)
 rpio.pwmSetClockDivider(64);
 rpio.pwmSetRange(IN1, 1024);
