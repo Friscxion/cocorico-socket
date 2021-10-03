deadc667a2d73a15adf303b0e54b5bcc27e51d6c
Friscxion <erwann.champion@outlook.fr>
2021-10-03 11:45:07 +0200

    Button

--- test.js
+++ test.js
@@ -4,7 +4,8 @@ const forward = new Gpio(14, {mode: Gpio.OUTPUT});
 const back = new Gpio(15, {mode: Gpio.OUTPUT});
 let dutyCycle = 0;
 
-while(true){
-    forward.pwmWrite(100);
-    back.pwmWrite(0);
-}
+forward.pwmWrite(100);
+back.pwmWrite(0);
+
+forward.start();
+back.start();
