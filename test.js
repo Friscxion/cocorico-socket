decaf9349d08a6ccddbba870c11980741d86abf9
Friscxion <erwann.champion@outlook.fr>
2021-10-10 12:08:14 +0200

    Button

--- test.js
+++ test.js
@@ -1,29 +1,17 @@
-/**
- * Sample for the motor-l298n.js.
- *
- * In this test we run the motors forward at 100%.
- */
-
-var speed      = 100; // Percentage
-var in1Pin     = 14;
-var in2Pin     = 15;
-var enable1Pin = 12;
 
 
-console.log("Starting motor-l298n - test_forward");
+let in1 = 14;
+let in2 = 15;
 
-var l298nModule = require("motor-l298n");
-var l298n = l298nModule.setup(in1Pin, in2Pin, enable1Pin);
-//console.log("Globals: LEFT=%d, RIGHT=%d", l298nModule.LEFT, l298nModule.RIGHT);
+const led = new Gpio(14, {mode: Gpio.OUTPUT});
 
-l298n.forward(l298nModule.LEFT);
-l298n.forward(l298nModule.RIGHT);
-l298n.setSpeed(l298nModule.LEFT,speed);
-l298n.setSpeed(l298nModule.RIGHT,speed);
-console.log("Moving forward!!");
+let dutyCycle = 0;
 
-setTimeout(function() {
-    l298n.stop(l298nModule.LEFT);
-    l298n.stop(l298nModule.RIGHT);
-    console.log("All done!");
-}, 180*1000);
+setInterval(() => {
+    led.pwmWrite(dutyCycle);
+
+    dutyCycle += 5;
+    if (dutyCycle > 255) {
+        dutyCycle = 0;
+    }
+}, 20);
