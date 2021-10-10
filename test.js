b0347fee5a81c272658c8d8bdffc7928813244ed
Friscxion <erwann.champion@outlook.fr>
2021-10-10 12:09:53 +0200

    Button

--- test.js
+++ test.js
@@ -7,12 +7,7 @@ let in2 = 15;
 const led = new Gpio(in1, {mode: Gpio.OUTPUT});
 
 let dutyCycle = 0;
-
+led.pwmWrite(100);
 setInterval(() => {
-    led.pwmWrite(dutyCycle);
 
-    dutyCycle += 5;
-    if (dutyCycle > 255) {
-        dutyCycle = 0;
-    }
 }, 20);
