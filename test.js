ca8cd553c87ef8026964c158a20ba78e73eee386
Friscxion <erwann.champion@outlook.fr>
2021-10-10 12:13:51 +0200

    Button

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const Gpio = require('pigpio').Gpio;
 let in1 = 14;
 let in2 = 15;
 
-const led = new Gpio(in1, {mode: Gpio.OUTPUT});
+const led = new Gpio(in2, {mode: Gpio.OUTPUT});
 
 let dutyCycle = 0;
 led.pwmWrite(255);
