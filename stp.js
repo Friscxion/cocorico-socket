daa27542f01ae824b38d925fa8e0ba1b503c4015
Friscxion <erwann.champion@outlook.fr>
2021-10-10 12:23:42 +0200

    Button

---
+++ stp.js
@@ -0,0 +1,9 @@
+
+const in1=14;
+const in2=15;
+const Gpio = require('pigpio').Gpio;
+const forward = new Gpio(in1, {mode: Gpio.OUTPUT});
+const back = new Gpio(in2, {mode: Gpio.OUTPUT});
+forward.pwmWrite(0);
+back.pwmWrite(0);
+
